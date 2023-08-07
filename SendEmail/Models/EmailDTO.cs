0573177c470c987557d16af507f86d6d5b9d35d8
alexisleomena <alexismena2690@gmail.com>
2023-08-07 15:51:21 -0300

    Contoller, Service and Model Email

---
+++ SendEmail/Models/EmailDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace SendEmail.Models
+{
+    public class EmailDTO
+    {
+        public string To { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+
+    }
+}
